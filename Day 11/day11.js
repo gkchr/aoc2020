5d7cff5219a8f86c23c591bc5099c7dfdf628e21
Gerhard Kocher <gerhard@cookers.at>
2020-12-11 10:07:32 +0100

    day 11 with many debug-outputs so far... just fyi

---
+++ Day 11/day11.js
@@ -0,0 +1,152 @@
+const fs = require('fs');
+
+
+
+/**
+ * read and prepare input and run the challenge.
+ */
+fs.readFile('input.txt', 'utf8', function (err, data) {
+    if (err) {
+        return console.error(err);
+    }
+
+    /**
+     * split input.
+     */
+    let input = data
+        .replace(/(\r\n|\n|\r)/gm, "\n")
+        .split("\n")
+        .filter(i => i)
+        .map(i => i.split(""));
+
+    console.log("--- part 1 ---", part1(input));
+
+    console.log("--- part 2 ---", part2(input));
+});
+
+
+
+/**
+ * the daily challenge, part 1.
+ *
+ * https://adventofcode.com/2020/day/11
+ */
+function part1(input) {
+    let output = step(input);
+    while(output.changed > 0) {
+        output = step(output.array);
+    }
+    let seats = output.array.reduce((count, row) => count + row.reduce((count, i) => count + (i === "#" ? 1 : 0), 0), 0);
+    console.log(seats);
+}
+
+function step(input) {
+    let output = [];
+    let changed = 0;
+
+    for(let i = 0; i < input.length; ++i) {
+        output[i] = [];
+        for(let j = 0; j < input[i].length; ++j) {
+            if(input[i][j] === ".") {
+                output[i][j] = ".";
+            } else {
+                let neighbors =
+                    (input[i-1] && input[i-1][j-1] && input[i-1][j-1] === "#" ? 1 : 0) +
+                    (input[i-1] && input[i-1][j]   && input[i-1][j]   === "#" ? 1 : 0) +
+                    (input[i-1] && input[i-1][j+1] && input[i-1][j+1] === "#" ? 1 : 0) +
+                    (              input[i][j-1]   && input[i][j-1]   === "#" ? 1 : 0) +
+                    (              input[i][j+1]   && input[i][j+1]   === "#" ? 1 : 0) +
+                    (input[i+1] && input[i+1][j-1] && input[i+1][j-1] === "#" ? 1 : 0) +
+                    (input[i+1] && input[i+1][j]   && input[i+1][j]   === "#" ? 1 : 0) +
+                    (input[i+1] && input[i+1][j+1] && input[i+1][j+1] === "#" ? 1 : 0);
+
+                if(input[i][j] === "#" && neighbors > 3) {
+                    output[i][j] = "L";
+                    changed ++;
+                } else if(input[i][j] === "L" && neighbors === 0) {
+                    output[i][j] = "#";
+                    changed ++;
+                } else {
+                    output[i][j] = input[i][j];
+                }
+            }
+        }
+    }
+    return {array: output, changed: changed};
+}
+
+
+
+/**
+ * the daily challenge, part 2.
+ *
+ * https://adventofcode.com/2020/day/11
+ */
+function part2(input) {
+    let output = step2(input);
+    while(output.changed > 0) {
+        output = step2(output.array);
+    }
+    let seats = output.array.reduce((count, row) => count + row.reduce((count, i) => count + (i === "#" ? 1 : 0), 0), 0);
+    console.log(seats);
+}
+
+function step2(input) {
+    let output = [];
+    let vis = [];
+    let changed = 0;
+
+    for(let i = 0; i < input.length; ++i) {
+        output[i] = [];
+        vis[i] = [];
+        for(let j = 0; j < input[i].length; ++j) {
+            if(input[i][j] === ".") {
+                output[i][j] = ".";
+                vis[i][j] = ". ";
+            } else {
+                let neighbors =
+                    isOccupied(input, i, j, -1, -1) +
+                    isOccupied(input, i, j, -1, 0 ) +
+                    isOccupied(input, i, j, -1, 1 ) +
+                    isOccupied(input, i, j, 0,  -1) +
+                    isOccupied(input, i, j, 0,  1 ) +
+                    isOccupied(input, i, j, 1,  -1) +
+                    isOccupied(input, i, j, 1,  0 ) +
+                    isOccupied(input, i, j, 1,  1 );
+
+                if(input[i][j] === "#" && neighbors > 4) {
+                    output[i][j] = "L";
+                    changed ++;
+                } else if(input[i][j] === "L" && neighbors === 0) {
+                    output[i][j] = "#";
+                    changed ++;
+                } else {
+                    output[i][j] = input[i][j];
+                }
+                vis[i][j] = input[i][j] + "" + neighbors;
+            }
+        }
+    }
+    console.log(vis.map(i => i.join(" ")));
+    console.log(changed)
+    return {array: output, changed: changed};
+}
+
+function isOccupied(arr, y, x, y_, x_) {
+    let interesting = {x: -1, y: -1};
+    for(let i = 1; ; ++i) {
+        if(x == interesting.x && y == interesting.y) console.log(i, x, y, arr[y][x], x_, y_);
+        if(arr[y+y_*i] && arr[y+y_*i][x+x_*i]) {
+            if(arr[y+y_*i][x+x_*i] === ".") {
+                continue;
+            }
+            if(arr[y+y_*i][x+x_*i] === "#") {
+                if(x == interesting.x && y == interesting.y) console.log("return 1 for ", y+y_*i, x+x_*i, "\n");
+
+                return 1;
+            }
+        }
+        if(x == interesting.x && y == interesting.y) console.log("return 0 for ", y+y_*i, x+x_*i, "\n");
+        return 0;
+    }
+}
