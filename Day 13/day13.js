ec021ca8968ef19a349a6add2790bfcf3e0dc2e2
Gerhard Kocher <gerhard@cookers.at>
2020-12-13 12:11:35 +0100

    Chinese remainder theorem (and reddit memes about it) saves the day.

--- Day 13/day13.js
+++ Day 13/day13.js
@@ -54,19 +54,20 @@ function part1(input) {
  * https://adventofcode.com/2020/day/13
  */
 function part2(input) {
-    let buses = input[1].split(",").map(Number).map((bus, i) => ({bus: bus, offset: i})).filter(bus => bus.bus).sort((a, b) => b.bus - a.bus);
+    let buses = input[1].split(",").map(Number).map((bus, i) => ({bus: bus, offset: i})).filter(bus => bus.bus);
 
-    let wrong = true;
-    let time = Infinity;
-    for(let mul = 218818380743; wrong; ++mul) {
-        time = buses[0].bus * mul - buses[0].offset;
-        wrong = false;
+    let time = 0;
+    let shift = buses.shift().bus;
 
-        for(let i = 1; i < buses.length; ++i) {
-            if((time + buses[i].offset) % buses[i].bus !== 0) wrong = true;
+    buses.forEach(bus => {
+        while (true) {
+            if ((time + bus.offset) % bus.bus === 0) {
+                shift *= bus.bus;
+                break;
+            }
+            time += shift;
         }
-    }
+    });
 
-    console.log(buses, time)
     return time;
 }
